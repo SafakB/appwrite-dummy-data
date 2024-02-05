3c5308a7ef375307e38270e7799b185f0e515108
SafakB <safakbahce@gmail.com>
2024-02-05 10:46:14 +0300

    init

---
+++ index.mjs
@@ -0,0 +1,34 @@
+import { Client, Databases } from "appwrite";
+
+const WAIT_TIME = 50;
+
+// Appwrite client setup
+let client = new Client();
+
+client
+    .setEndpoint('https://localhost/v1') // Appwrite server endpoint
+    .setProject('65ba1d991a2aec5b2374'); // Project ID
+
+let database = new Databases(client);
+
+// Dummy data count
+const count = 250000;
+
+for (let i = 0; i < count; i++) {
+    let dummyData2 =
+    {
+        title: "Example Name " + i,
+        description: "Example Description" + i,
+        content: "Example Content" + i,
+        enabled: i % 2 == 0 ? true : false,
+    };
+
+    // Create a new document to the collection
+    database.createDocument('65ba4d642a413bcc21f6', '65bc11b2be70c218fc9e', 'unique()', dummyData2)
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
+    /* wait delay */
+    await new Promise(resolve => setTimeout(resolve, WAIT_TIME));
+}
+
+
