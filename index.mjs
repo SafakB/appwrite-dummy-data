4af0f2d8c9654b6dd9973015a080a6c9bac0a558
SafakB <safakbahce@gmail.com>
2024-05-01 18:49:23 +0300

    added random numbers and names

--- index.mjs
+++ index.mjs
@@ -1,4 +1,10 @@
 import { Client, Databases } from "appwrite";
+import { readFileSync } from 'fs';
+
+const rawData = readFileSync('first-names.json');
+const rawData1 = readFileSync('middle-names.json');
+const names = JSON.parse(rawData);
+const lastNames = JSON.parse(rawData1);
 
 const WAIT_TIME = 50;
 
@@ -7,20 +13,21 @@ let client = new Client();
 
 client
     .setEndpoint('https://localhost/v1') // Appwrite server endpoint
-    .setProject('65ba1d991a2aec5b2374'); // Project ID
+    .setProject('6632574e002f32c908b3'); // Project ID
 
 let database = new Databases(client);
 
 // Dummy data count
-const count = 250000;
+const count = 1000000;
 
 for (let i = 0; i < count; i++) {
+    let randomNumber = Math.floor(Math.random() * 10000);
     let dummyData =
     {
-        title: "Example Name " + i,
-        description: "Example Description" + i,
-        content: "Example Content" + i,
-        enabled: i % 2 == 0 ? true : false,
+        title: rastgeleIsim(names) + " " + rastgeleIsim(lastNames),
+        description: "Example Description " + i,
+        content: "Example Content " + randomNumber,
+        enabled: randomNumber % 2 == 0 ? true : false,
     };
 
     // Create a new document to the collection
@@ -31,4 +38,7 @@ for (let i = 0; i < count; i++) {
     await new Promise(resolve => setTimeout(resolve, WAIT_TIME));
 }
 
-
+function rastgeleIsim(isimler) {
+    const rastgeleIndex = Math.floor(Math.random() * isimler.length);
+    return isimler[rastgeleIndex];
+}
